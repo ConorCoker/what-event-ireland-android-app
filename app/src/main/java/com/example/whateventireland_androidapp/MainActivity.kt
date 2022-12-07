ba6d3732bb864f31fe6717d9fe1b9650f2417cf3
Conor Coker <conorcoker@gmail.com>
2022-12-07 22:03:16 +0000

    Created new project worked on homescreen layout,view binding and click listener to move to the register screen.

---
+++ app/src/main/java/com/example/whateventireland_androidapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.whateventireland_androidapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.whateventireland_androidapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupOnClickListeners()
+    }
+
+    private fun setupOnClickListeners(){
+        binding.buttonRegister.setOnClickListener {
+            val registerIntent = Intent(this,RegisterActivity::class.java)
+            startActivity(registerIntent)
+            setContentView(R.layout.activity_register)
+        }
+
+    }
+}
